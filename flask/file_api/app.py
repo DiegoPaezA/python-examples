be942e4523625f881571cb4584e196a8c55e4a7a
diegopaezasus <diripar8@gmail.com>
2023-03-25 16:52:31 -0300

    update file_path and list of files

--- flask/file_api/app.py
+++ flask/file_api/app.py
@@ -1,36 +1,41 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import helper_function as hf
+from dummy_data import films, images
 import glob
-
+import random
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+
 # check if file.txt exists if not empty list
-list_files = glob.glob('*.txt') # list of files created
+list_files = glob.glob('files/*') # list of files created
 
 @app.route("/") # home page
-def home():
-    return "Hello, This is the first Task: Create an API with Flask"
+def index():
+    url = random.choice(images)
+    return render_template("index.html", url=url)
 
 # Endpoint to create a file
 @app.route("/create_file", methods=["POST"])
 def create_file():
     post_res = request.get_json()
     filename = post_res.get("file_name")
+    filename_path = "files/" + filename
     
-    if hf.check_file_exists(filename):
+    if hf.check_file_exists(filename_path):
         return jsonify(
             Mensagem= "Arquivo ja existe, por favor escolha outro nome",
             Arquivos = list_files)
 
     list_files.append(filename)
-    with open(filename, "w") as f:
-        f.write(f"O Arquivo criado foi {filename}")
+    with open(filename_path, "w") as f:
+        print(f"O Arquivo criado foi {filename}")
     f.close()    
     return jsonify(
         Mensagem= "Lista de Arquivos Criados",
         Arquivos = list_files)
+    
     
 # Endpoint to list all the files created   
 @app.route("/file_list", methods=["GET"])
@@ -45,13 +50,14 @@ def read_filelist():
 def read_file():
     post_res = request.get_json()
     filename = post_res.get("file_name")
+    filename_path = "files/" + filename
     
-    if hf.check_file_exists(filename) == False:
+    if hf.check_file_exists(filename_path) == False:
         return jsonify(
             Mensagem= "Arquivo nao existe, por favor escolha outro nome",
             Arquivos = list_files)
         
-    with open(filename, "r") as f:
+    with open(filename_path, "r") as f:
         contents = f.readlines()
     f.close()
         
@@ -65,16 +71,17 @@ def update_file():
     post_res = request.get_json()
     filename = post_res.get("file_name")
     message = post_res.get("message")
+    filename_path = "files/" + filename
     
-    if hf.check_file_exists(filename) == False:
+    if hf.check_file_exists(filename_path) == False:
         return jsonify(
             Mensagem= "Arquivo nao existe, por favor escolha outro nome",
             Arquivos = list_files)
     
-    with open(filename, "a") as f:
+    with open(filename_path, "a") as f:
         f.write(f" {message}")
     f.close()
-    with open(filename, "r") as f:
+    with open(filename_path, "r") as f:
         contents = f.readlines()
     f.close()    
     return jsonify(
