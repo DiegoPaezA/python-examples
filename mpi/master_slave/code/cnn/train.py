f529a50d5ba74e563ba2546eb05ec1689f997534
DiegoPaez <diripar8@gmail.com>
2023-04-17 14:48:29 -0300

    remove load dataset from calculate fitness

--- mpi/master_slave/code/cnn/train.py
+++ mpi/master_slave/code/cnn/train.py
@@ -16,20 +16,22 @@ mixed_precision.set_global_policy('mixed_float16')
 # Disable tensorflow logging
 hf.disable_tf_logging()
 
-def calculate_fitness(net, params):
+def calculate_fitness(train_data, test_data, num_classes, net, params):
     """
     This function calculates the fitness of a model
 
     Args:
+        train_data (tuple): tuple with the training data
+        test_data (tuple): tuple with the test data
+        num_classes (int): number of classes
         net (dict): dictionary with the CNN architecture
         params (dict): dictionary with the CNN training parameters
 
     Returns:
         fitness (float): fitness of the model
     """
-    # Load data - CIFAR10
-    num_classes = 10
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train, y_train = train_data
+    x_test, y_test = test_data
     # Normalization is donde in create vgg function
     model = create_vgg(x_train.shape[1:], num_classes, net)
     
@@ -41,7 +43,7 @@ def calculate_fitness(net, params):
     print("Training model...")
     history = model.fit(x_train, y_train, 
               epochs=params['epochs'],
-              validation_data=(x_test, y_test),
+              validation_data=test_data,
               batch_size=params['batch_size'], 
               verbose=1)
     print("Model trained")
@@ -52,16 +54,24 @@ def calculate_fitness(net, params):
 
 if __name__ == '__main__':
     # Read parameters
+    
+    # Load data - CIFAR10
+    num_classes = 10
+    train_data, test_data = keras.datasets.cifar10.load_data()
+    
+    # Load nets and params
     net = hf.load_nets("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_nets.yml")
     params = hf.load_params("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_params.yml")  
     fitness_results = []
     history_results = []
+    
     # Calculate fitness
     for i in range(len(net)):
-        fitness, history = calculate_fitness(net[i], params[0])
+        fitness, history = calculate_fitness(train_data,test_data,num_classes,net[1], params[0])
         fitness_results.append(fitness)
         history_results.append(history)
         print(f"Fitness of Net_{i+1}: {fitness}")
         
+    print("n/ Results Resume:")    
     for i in range(len(fitness_results)):
-        print(f"Fitness of Net_{i+1}: {fitness_results[i]}")    
+        print(f"Fitness of Net_{i+1}: {fitness_results[i]}")
