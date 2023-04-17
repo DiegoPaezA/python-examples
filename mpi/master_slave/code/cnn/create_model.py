39cdbf68530cd36a27054796c561e6e7fb981f36
DiegoPaez <diripar8@gmail.com>
2023-04-17 15:50:14 -0300

    add utils functions

--- mpi/master_slave/code/cnn/create_model.py
+++ mpi/master_slave/code/cnn/create_model.py
@@ -1,7 +1,6 @@
 #Importing required libararies
 import sys
-sys.path.insert(1, '/home/diripar8/python-examples/mpi/master_slave/code/')
-import helper_functions as hf
+from utils import disable_tf_logging
 
 import tensorflow as tf
 from tensorflow import keras
@@ -11,7 +10,7 @@ from tensorflow.keras.layers.experimental import preprocessing
 
 
 # Disable tensorflow logging
-hf.disable_tf_logging()
+disable_tf_logging()
 
 def vgg_block(layer_in, n_filters, n_conv):
     """
@@ -84,25 +83,3 @@ def create_vgg(input_shape, num_classes, net):
 
 # if __name__ == '__main__':
     
-#     net = hf.load_nets("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_nets.yml")
-#     params = hf.load_params("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_params.yml")
-#     params = params[0]
-#     num_classes = 10
-#     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-
-#     # Normalization is donde in create vgg function
-#     model = create_vgg(x_train.shape[1:], num_classes, net[0])
-#     print(model.summary())
-    
-#     model.compile(optimizer=keras.optimizers.Adam(learning_rate=params['lr']),
-#                   loss=params['loss'],
-#                   metrics=['accuracy'])
-    
-#     # # Train model
-#     print("Training model...")
-#     model.fit(x_train, y_train, 
-#               epochs=params['epochs'],
-#               validation_data=(x_test, y_test),
-#               batch_size=params['batch_size'], 
-#               verbose=1)
-#     print("Model trained")
--- mpi/master_slave/code/cnn/train.py
+++ mpi/master_slave/code/cnn/train.py
@@ -1,20 +1,16 @@
 #Importing required libararies
-import sys
-sys.path.insert(1, '/home/diripar8/python-examples/mpi/master_slave/code/')
-import helper_functions as hf
+from utils import disable_tf_logging
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import mixed_precision
 from create_model import create_vgg
 
-import matplotlib.pyplot as plt
-
 # Enable mixed precision
 mixed_precision.set_global_policy('mixed_float16')
 
 # Disable tensorflow logging
-hf.disable_tf_logging()
+disable_tf_logging()
 
 def calculate_fitness(train_data, test_data, num_classes, net, params):
     """
@@ -34,44 +30,41 @@ def calculate_fitness(train_data, test_data, num_classes, net, params):
     x_test, y_test = test_data
     # Normalization is donde in create vgg function
     model = create_vgg(x_train.shape[1:], num_classes, net)
-    
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=params['lr']),
                   loss=params['loss'],
                   metrics=['accuracy'])
-    
     # Train model
     print("Training model...")
     history = model.fit(x_train, y_train, 
               epochs=params['epochs'],
               validation_data=test_data,
               batch_size=params['batch_size'], 
-              verbose=1)
+              verbose=0)
     print("Model trained")
-
     # Evaluate model
     _, fitness = model.evaluate(x_test, y_test, verbose=0)
     return fitness, history;
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # Read parameters
     
     # Load data - CIFAR10
-    num_classes = 10
-    train_data, test_data = keras.datasets.cifar10.load_data()
+    # num_classes = 10
+    # train_data, test_data = keras.datasets.cifar10.load_data()
     
-    # Load nets and params
-    net = hf.load_nets("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_nets.yml")
-    params = hf.load_params("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_params.yml")  
-    fitness_results = []
-    history_results = []
+    # # Load nets and params
+    # net = hf.load_nets("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_nets.yml")
+    # params = hf.load_params("/home/diripar8/python-examples/mpi/master_slave/code/cnn_nets/cnn_params.yml")  
+    # fitness_results = []
+    # history_results = []
     
-    # Calculate fitness
-    for i in range(len(net)):
-        fitness, history = calculate_fitness(train_data,test_data,num_classes,net[1], params[0])
-        fitness_results.append(fitness)
-        history_results.append(history)
-        print(f"Fitness of Net_{i+1}: {fitness}")
+    # # Calculate fitness
+    # for i in range(len(net)):
+    #     fitness, history = calculate_fitness(train_data,test_data,num_classes,net[1], params[0])
+    #     fitness_results.append(fitness)
+    #     history_results.append(history)
+    #     print(f"Fitness of Net_{i+1}: {fitness}")
         
-    print("n/ Results Resume:")    
-    for i in range(len(fitness_results)):
-        print(f"Fitness of Net_{i+1}: {fitness_results[i]}")
+    # print("\n Results Resume:")    
+    # for i in range(len(fitness_results)):
+    #     print(f"Fitness of Net_{i+1}: {fitness_results[i]}")
