1d78217c78ca93820192fc86503f84fbeb37f47d
DiegoPaez <diripar8@gmail.com>
2023-05-23 19:11:38 +0000

    load cifar

--- docker/tf_mpi/code/cnn/utils.py
+++ docker/tf_mpi/code/cnn/utils.py
@@ -1,4 +1,7 @@
 import os 
+import numpy as np
+from six.moves import cPickle as pickle
+import platform
 
 def disable_tf_logging(level:int=1):
     """
@@ -11,4 +14,38 @@ def disable_tf_logging(level:int=1):
         2 = INFO with WARNINGS is removed.
         3= ALL messages are removed.
     """
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(level) 
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(level)
+    
+
+def load_pickle(f):
+    version = platform.python_version_tuple()
+    if version[0] == '2':
+        return  pickle.load(f)
+    elif version[0] == '3':
+        return  pickle.load(f, encoding='latin1')
+    raise ValueError("invalid python version: {}".format(version))
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000,3072)
+        Y = np.array(Y)
+        return X, Y
+
+def load_CIFAR10(ROOT):
+    """ load all of cifar """
+    xs = []
+    ys = []
+    for b in range(1,6):
+        f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
